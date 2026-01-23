934c815d20d91253e0dbfaaf2ffb617f92c97495
Tai Hoang <dev@convertdigital.com.au>
2026-01-23 11:30:23 +0700

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: "aka-ecommerce",
+      name: "aka-home",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
+      args: "start -p 3001",
       exec_mode: "cluster",
       instances: "max",
       watch: false,
@@ -14,8 +14,8 @@ module.exports = {
         PORT: 3000,
       },
       // Add log rotation and better restart strategy
-      error_file: "/opt/webapps/aka/logs/error.log",
-      out_file: "/opt/webapps/aka/logs/out.log",
+      error_file: "/opt/webapps/aka-home/logs/error.log",
+      out_file: "/opt/webapps/aka-home/logs/out.log",
       merge_logs: true,
       log_date_format: "YYYY-MM-DD HH:mm Z",
       // Graceful reload (no downtime)
